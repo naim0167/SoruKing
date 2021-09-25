80021bea3ae33d6b0693c122a593f132fab8ed9d
Md. Naim Chowdhury <naimmcduck@gmail.com>
2021-09-26 01:32:30 +0200

    test event

--- src/events.js
+++ src/events.js
@@ -1,27 +1,27 @@
-player.on('error', (queue, error) => {
-    console.log(`Error emitted from the queue ${error.message}`);
-});
+// player.on('error', (queue, error) => {
+//     console.log(`Error emitted from the queue ${error.message}`);
+// });
 
-player.on('connectionError', (queue, error) => {
-    console.log(`Error emitted from the connection ${error.message}`);
-});
+// player.on('connectionError', (queue, error) => {
+//     console.log(`Error emitted from the connection ${error.message}`);
+// });
 
-player.on('trackStart', (queue, track) => {
-    queue.metadata.send(`Started playing ${track.title} in **${queue.connection.channel.name}** 🎧`);
-});
+// player.on('trackStart', (queue, track) => {
+//     queue.metadata.send(`Started playing ${track.title} in **${queue.connection.channel.name}** 🎧`);
+// });
 
-player.on('trackAdd', (queue, track) => {
-    queue.metadata.send(`${track.title} এই সঙ্গীতখানা কিউতে ঢুকায় দেওয়া হইল ✅`);
-});
+// player.on('trackAdd', (queue, track) => {
+//     queue.metadata.send(`${track.title} এই সঙ্গীতখানা কিউতে ঢুকায় দেওয়া হইল ✅`);
+// });
 
-player.on('botDisconnect', (queue) => {
-    queue.metadata.send('কেডা জানি আমারে মাইরা বাইর করে দিসে 😭, লিস্ট ফাকা চকচকা... ❌');
-});
+// player.on('botDisconnect', (queue) => {
+//     queue.metadata.send('কেডা জানি আমারে মাইরা বাইর করে দিসে 😭, লিস্ট ফাকা চকচকা... ❌');
+// });
 
-player.on('channelEmpty', (queue) => {
-    queue.metadata.send('আমারে একলা রাইখা সব জানি কই গেসে, যাইগা আমিও... ❌');
-});
+// player.on('channelEmpty', (queue) => {
+//     queue.metadata.send('আমারে একলা রাইখা সব জানি কই গেসে, যাইগা আমিও... ❌');
+// });
 
-player.on('queueEnd', (queue) => {
-    queue.metadata.send('লিস্টের সব সঙ্গীত শেষ, শেখ হাসিনার বাংলাদেশ ✅');
-});
+// player.on('queueEnd', (queue) => {
+//     queue.metadata.send('লিস্টের সব সঙ্গীত শেষ, শেখ হাসিনার বাংলাদেশ ✅');
+// });
