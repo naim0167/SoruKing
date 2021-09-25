a4bc75c2a939d04027bdbfe9e12349d402a006d3
Md. Naim Chowdhury <naimmcduck@gmail.com>
2021-09-25 22:35:48 +0200

    Add license and cofig

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    app: {
+        px: '-',
+        token: 'ODkxMjU2MTM3MjkzMzgxNjcy.YU7s9Q.lJHac887Eq_SppSYqpr8DBeG9Q4',
+        playing: 'by Yescoders ❤️',
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        discordPlayer: {}
+    }
+};
