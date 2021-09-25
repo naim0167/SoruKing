7d9ed297a2875a228257d39b09ac2b4f6ea143f4
Md. Naim Chowdhury <naimmcduck@gmail.com>
2021-09-26 01:49:06 +0200

    revert to old state

--- src/events.js
+++ src/events.js
@@ -6,22 +6,22 @@ player.on('connectionError', (queue, error) => {
     console.log(`Error emitted from the connection ${error.message}`);
 });
 
-player.on('trackStart', (queue, track) => {
-    queue.metadata.send(`Started playing ${track.title} 🎧`);
-});
+// player.on('trackStart', (queue, track) => {
+//     queue.metadata.send(`Started playing ${track.title} in **${queue.connection.channel.name}** 🎧`);
+// });
 
 // player.on('trackAdd', (queue, track) => {
 //     queue.metadata.send(`${track.title} এই সঙ্গীতখানা কিউতে ঢুকায় দেওয়া হইল ✅`);
 // });
 
-player.on('botDisconnect', (queue) => {
-    queue.metadata.send('কেডা জানি আমারে মাইরা বাইর করে দিসে 😭, লিস্ট ফাকা চকচকা... ❌');
-});
+// player.on('botDisconnect', (queue) => {
+//     queue.metadata.send('কেডা জানি আমারে মাইরা বাইর করে দিসে 😭, লিস্ট ফাকা চকচকা... ❌');
+// });
 
 // player.on('channelEmpty', (queue) => {
 //     queue.metadata.send('আমারে একলা রাইখা সব জানি কই গেসে, যাইগা আমিও... ❌');
 // });
 
 player.on('queueEnd', (queue) => {
-    queue.metadata.send('লিস্টের সকল সঙ্গীত শেষ, শেখ হাসিনার বাংলাদেশ 🇧🇩');
+    queue.metadata.send('লিস্টের সকব সঙ্গীত শেষ, শেখ হাসিনার বাংলাদেশ 🇧🇩');
 });
