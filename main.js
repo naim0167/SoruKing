d6106502e2fd98f433e5fb4d3ffc0961f671f24c
Md. Naim Chowdhury <naimmcduck@gmail.com>
2021-09-25 23:04:33 +0200

    commented config

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ global.client = new Client({
     disableMentions: 'everyone',
 });
 
-client.config = require('./config');
+// client.config = require('./config');
 
 global.player = new Player(client, client.config.opt.discordPlayer);
 
